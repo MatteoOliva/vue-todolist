43aa6db918ee56e3c3e6d06fa9b8c40135ad144e
MatteoOliva <oliva.matteo98@outlook.com>
2024-01-31 13:42:43 +0100

    feat:stampato la lista

--- script.js
+++ script.js
@@ -2,7 +2,30 @@ const {createApp} = Vue;
 
 createApp ({
     data() {
-      return
+      return {
+        tasks: [
+            {
+                text: 'Andare in Palestra',
+                done: true,
+            },
+            {
+                text: 'Studiare',
+                done: false,
+            },
+            {
+                text: 'Fare la spesa',
+                done: true,
+            },
+            {
+                text: 'Annaffiare le piante',
+                done: true,
+            },
+            {
+                text: 'Pulire la casa',
+                done: false,
+            },
+        ]
+      }
         
     },
   }).mount('#app')
