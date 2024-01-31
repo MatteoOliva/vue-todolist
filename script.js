02c7874ff6b386bc22fd19f11e92b5c12f7a6f2d
MatteoOliva <oliva.matteo98@outlook.com>
2024-01-31 13:57:26 +0100

    feat: Milestone1

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-const {createApp} = Vue;
+const { createApp } = Vue;
 
-createApp ({
+createApp({
     data() {
-      return {
-        tasks: [
-            {
-                text: 'Andare in Palestra',
-                done: true,
-            },
-            {
-                text: 'Studiare',
-                done: false,
-            },
-            {
-                text: 'Fare la spesa',
-                done: true,
-            },
-            {
-                text: 'Annaffiare le piante',
-                done: true,
-            },
-            {
-                text: 'Pulire la casa',
-                done: false,
-            },
-        ]
-      }
-        
+        return {
+            tasks: [
+                {
+                    text: 'Andare in Palestra',
+                    done: true,
+                },
+                {
+                    text: 'Studiare',
+                    done: false,
+                },
+                {
+                    text: 'Fare la spesa',
+                    done: true,
+                },
+                {
+                    text: 'Annaffiare le piante',
+                    done: true,
+                },
+                {
+                    text: 'Pulire la casa',
+                    done: false,
+                },
+            ]
+        }
+
     },
-  }).mount('#app')
+}).mount('#app')
